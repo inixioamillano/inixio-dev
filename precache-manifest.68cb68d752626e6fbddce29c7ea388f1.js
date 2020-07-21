6ee19fb17a9ef86207b05c2e6d2603f76519866f
Inixio <inixio97@gmail.com>
2020-07-21 18:13:35 +0200

    Updates

---
+++ precache-manifest.68cb68d752626e6fbddce29c7ea388f1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eef694e5fede66f5960544f322693f96",
+    "url": "/inixio-dev/index.html"
+  },
+  {
+    "revision": "0adb9794391b52bafe8f",
+    "url": "/inixio-dev/static/css/2.e53f431d.chunk.css"
+  },
+  {
+    "revision": "7db52c3e4466d68901ea",
+    "url": "/inixio-dev/static/css/main.0250cdc9.chunk.css"
+  },
+  {
+    "revision": "0adb9794391b52bafe8f",
+    "url": "/inixio-dev/static/js/2.7f428f60.chunk.js"
+  },
+  {
+    "revision": "7ac6958339c84b35bcc5382b5de3b4a3",
+    "url": "/inixio-dev/static/js/2.7f428f60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7db52c3e4466d68901ea",
+    "url": "/inixio-dev/static/js/main.f3602a14.chunk.js"
+  },
+  {
+    "revision": "872664fd0ac1e00f464a",
+    "url": "/inixio-dev/static/js/runtime-main.34fec000.js"
+  },
+  {
+    "revision": "60893e172836ac8a1e7bd6b90930a41a",
+    "url": "/inixio-dev/static/media/CodenameCoderFree4F-Bold.60893e17.woff"
+  },
+  {
+    "revision": "e39e0d69e6dd4e61ed748c141882a441",
+    "url": "/inixio-dev/static/media/CodenameCoderFree4F-Bold.e39e0d69.woff2"
+  }
+]);
